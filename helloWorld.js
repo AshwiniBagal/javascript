efd2b453e6c05d13e5b75c40422229ffc244e9b2
AshwiniBagal <bashwini70@gmail.com>
2023-12-26 13:59:55 +0530

    Basic Javascript
    Basic Javascript code

---
+++ helloWorld.js
@@ -0,0 +1,34 @@
+// console.log("Hello JS, I am Ashwini");
+// // Naming conventions : 1.camalCase 2.Pascalcase 3 .snack_case 4.kebab-cas
+
+// var num;// variale declaration
+// console.log(num);// undefined
+// var num = "10";   // var declaration & initialization at same line . most use it
+// num="10";         //variable initalization
+// console.log(num);
+
+var myName = "Ashwini";
+
+console.log("First Name:",  myName)
+
+myName="Ashwini Bagal"; //Update Value
+console.log("Updated Name: ");
+console.log("Full Name:",  myName);
+
+ var myLove = "I Love only Js";
+ console.log(myLove);
+ var firstName = "Ashwini";
+ var lastName = "Bagal"
+ var age = 26;
+ var address = "Pandharpur, Maharashtra";
+ var pinCode = 413304;
+ var gender = "female";
+ var isMarried = "No";
+
+ console.log("First Name:",firstName);
+ console.log("Last Name:", lastName);
+console.log( "Age:", age);
+console.log( "Address:" , address);
+console.log ("Pin Code:",pinCode); 
+console.log("Gender:", gender);
+console.log( "IsMarried:",isMarried);
